5639487d8af6a06343940c7e5dbdd5662d485252
Evans <evanskipngetichsteel@gmail.com>
2022-01-24 23:05:04 +0300

    Updated changes

--- group-units.py
+++ group-units.py
@@ -13,7 +13,7 @@ z = 1
 # Iterating through all the files in our folder
 # create the major folder here
 save_path = 'H:/WORK/Upwork/Project 7 - Python School Data Analysis/Countries Admin Levels/GADM-Countries/Extracted/Admin Data GeoJSON/'
-
+country_group = {}
 for file in os.listdir():
     # create a sub-folder for geojson files
     sub_path = os.path.join(save_path, file)
@@ -28,61 +28,62 @@ for file in os.listdir():
         data = json.load(f)
         
     n = ''
-    country_group = {}
+    
     
     # filter
     if file_count == 1:
         print(file, '1 file was found')
         for item in data['features']:
             if n:
-                b = item['properties']['NAME_1']
-                c = item['properties']['NAME_2']
-                d = item['properties']['NAME_3']
-                country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                continue
+                # b = item['properties']['NAME_1']
+                # c = item['properties']['NAME_2']
+                # d = item['properties']['NAME_3']
+                # country_group[n][b].append(c)
+                # country_group[n][b].append({'Adm_2':c,'Adm_3':d})
             else:
                 n = item['properties']['NAME_0']
                 country_group[n] = defaultdict(list)
-                b = item['properties']['NAME_1']
-                c = item['properties']['NAME_2']
-                d = item['properties']['NAME_3']
-                country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                # b = item['properties']['NAME_1']
+                # c = item['properties']['NAME_2']
+                # d = item['properties']['NAME_3']
+                # country_group[n][b].append(c)
+                # country_group[n][b].append({'Adm_2':c,'Adm_3':d})
                 
     elif file_count == 2:
         print(file, '2 files were found')
         for item in data['features']:
             if n:
                 b = item['properties']['NAME_1']
-                c = item['properties']['NAME_2']
-                d = item['properties']['NAME_3']
-                country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                # c = item['properties']['NAME_2']
+                # d = item['properties']['NAME_3']
+                country_group[n][b].append({'Adm_1': b})
+                #country_group[n][b].append({'Adm_2':c,'Adm_3':d})
             else:
                 n = item['properties']['NAME_0']
                 country_group[n] = defaultdict(list)
                 b = item['properties']['NAME_1']
-                c = item['properties']['NAME_2']
-                d = item['properties']['NAME_3']
-                country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                # c = item['properties']['NAME_2']
+                # d = item['properties']['NAME_3']
+                country_group[n][b].append({'Adm_1': b})
+                #country_group[n][b].append({'Adm_2':c,'Adm_3':d})
     elif file_count == 3:
         print(file, '3 files were found')
         for item in data['features']:
             if n:
                 b = item['properties']['NAME_1']
                 c = item['properties']['NAME_2']
-                d = item['properties']['NAME_3']
-                country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                #d = item['properties']['NAME_3']
+                #country_group[n][b].append(c)
+                country_group[n][b].append({'Adm_2':c})
             else:
                 n = item['properties']['NAME_0']
                 country_group[n] = defaultdict(list)
                 b = item['properties']['NAME_1']
                 c = item['properties']['NAME_2']
-                d = item['properties']['NAME_3']
-                country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                #d = item['properties']['NAME_3']
+                #country_group[n][b].append(c)
+                country_group[n][b].append({'Adm_2':c})
     elif file_count == 4:
         print(file, '4 files were found')
         for item in data['features']:
@@ -91,7 +92,7 @@ for file in os.listdir():
                 c = item['properties']['NAME_2']
                 d = item['properties']['NAME_3']
                 country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                country_group[n][b].append({'Adm_2':c,'Adm_3':d})
             else:
                 n = item['properties']['NAME_0']
                 country_group[n] = defaultdict(list)
@@ -99,7 +100,7 @@ for file in os.listdir():
                 c = item['properties']['NAME_2']
                 d = item['properties']['NAME_3']
                 country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                country_group[n][b].append({'Adm_2':c,'Adm_3':d})
     elif file_count == 5:
         print(file, '5 files were found')
         for item in data['features']:
@@ -107,16 +108,18 @@ for file in os.listdir():
                 b = item['properties']['NAME_1']
                 c = item['properties']['NAME_2']
                 d = item['properties']['NAME_3']
+                e = item['properties']['NAME_4']
                 country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                country_group[n][b].append({'Adm_2':c,'Adm_3':d, 'Adm_4':e})
             else:
                 n = item['properties']['NAME_0']
                 country_group[n] = defaultdict(list)
                 b = item['properties']['NAME_1']
                 c = item['properties']['NAME_2']
                 d = item['properties']['NAME_3']
+                e = item['properties']['NAME_4']
                 country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                country_group[n][b].append({'Adm_2':c,'Adm_3':d, 'Adm_4':e})
     elif file_count == 6:
         print(file, '6 files were found')
         for item in data['features']:
@@ -124,34 +127,38 @@ for file in os.listdir():
                 b = item['properties']['NAME_1']
                 c = item['properties']['NAME_2']
                 d = item['properties']['NAME_3']
+                e = item['properties']['NAME_4']
+                f = item['properties']['NAME_5']
                 country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                country_group[n][b].append({'Adm_2':c,'Adm_3':d, 'Adm_4':e, 'Adm_5':f})
             else:
                 n = item['properties']['NAME_0']
                 country_group[n] = defaultdict(list)
                 b = item['properties']['NAME_1']
                 c = item['properties']['NAME_2']
                 d = item['properties']['NAME_3']
+                e = item['properties']['NAME_4']
+                f = item['properties']['NAME_5']
                 country_group[n][b].append(c)
-                country_group[n][b][c].append(d)
+                country_group[n][b].append({'Adm_2':c,'Adm_3':d, 'Adm_4':e, 'Adm_5':f})
 
 
 # Reference Code
-n = ''
-country_group = {}
-for item in data['features']:
-    if n:
-        b = item['properties']['NAME_1']
-        c = item['properties']['NAME_2']
-        d = item['properties']['NAME_3']
-        country_group[n][b].append({'Adm_2':c,'Adm_3':d})
-        #country_group[n]['Adm_3'].append(d)
-    else:
-        n = item['properties']['NAME_0']
-        country_group[n] = defaultdict(list)
-        b = item['properties']['NAME_1']
-        c = item['properties']['NAME_2']
-        d = item['properties']['NAME_3']
-        #country_group[n][b].append(c)
-        country_group[n][b].append({'Adm_2':c,'Adm_3':d})
-        #country_group[n]['Adm_3'].append(d)
+# n = ''
+# country_group = {}
+# for item in data['features']:
+#     if n:
+#         b = item['properties']['NAME_1']
+#         c = item['properties']['NAME_2']
+#         d = item['properties']['NAME_3']
+#         country_group[n][b].append({'Adm_2':c,'Adm_3':d})
+#         #country_group[n]['Adm_3'].append(d)
+#     else:
+#         n = item['properties']['NAME_0']
+#         country_group[n] = defaultdict(list)
+#         b = item['properties']['NAME_1']
+#         c = item['properties']['NAME_2']
+#         d = item['properties']['NAME_3']
+#         #country_group[n][b].append(c)
+#         country_group[n][b].append({'Adm_2':c,'Adm_3':d})
+#         #country_group[n]['Adm_3'].append(d)
