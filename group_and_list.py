af25fac6fdf3958e0a2d757fd07cbf635da947b8
Evans <evanskipngetichsteel@gmail.com>
2022-01-25 16:35:39 +0300

    File changes and additions

--- group_and_list.py
+++ group_and_list.py
@@ -19,12 +19,7 @@ for file in os.listdir():
     sub_path = os.path.join(save_path, file)
     sub_path = sub_path + '/'
     for items in os.listdir(sub_path):
-        subb_path, sub_dirs, sub_files = next(os.walk(sub_path))
-        if len(sub_files) == 3:
-            
         path_to_geojson = os.path.join(sub_path, items)
-        # we can use here to parse previous files to get type of admin level
-        
     path, dirs, files = next(os.walk(sub_path))
     file_count = len(files)
     # this is where the reading happens:
@@ -72,11 +67,6 @@ for file in os.listdir():
                 # d = item['properties']['NAME_3']
                 country_group[n][b].append({'Adm_1': b})
                 #country_group[n][b].append({'Adm_2':c,'Adm_3':d})
-        admin_level1_units = len(country_group[n].keys())
-        admin_level1_names = item['properties']['ENGTYPE_1']
-        country_name = file
-        lowest_admin_level = file_count - 1
-        
     
     elif file_count == 3:
         print(file, '3 files were found')
@@ -95,12 +85,6 @@ for file in os.listdir():
                 #d = item['properties']['NAME_3']
                 #country_group[n][b].append(c)
                 country_group[n][b].append({'Adm_2':c})
-        admin_level1_units = len(country_group[n].keys())
-        #admin_level1_names = item['properties']['ENGTYPE_1']
-        country_name = file
-        lowest_admin_level = file_count - 1
-        # admin_level2_units 
-        admin_level2_names = item['properties']['ENGTYPE_2']
                 
     elif file_count == 4:
         print(file, '4 files were found')
@@ -182,34 +166,3 @@ for file in os.listdir():
 #         #country_group[n][b].append(c)
 #         country_group[n][b].append({'Adm_2':c,'Adm_3':d})
 #         #country_group[n]['Adm_3'].append(d)
-
-
-# adm_2_list = []
-# z = 0
-# for i in country_group['United Kingdom']['England']:
-#     if i['Adm_2'] not in adm_2_list:
-#         adm_2_list.append(i['Adm_2'])
-#         z += 1
-
-# if len(sub_files) == 4:
-#     province_sub_1 = sub_files[1]
-#     district_sub_2 = sub_files[2]
-#     sub_county_3 = sub_files[3]
-#     province_path = os.path.join(sub_path, province_sub_1)
-#     district_path = os.path.join(sub_path, district_sub_2)
-#     temp_list = [province_path, district_path]
-#     for i in temp_list:
-#         with open(i) as f:
-#             data = json.load(f)
-#         type_adm_1_list = []
-#         type_adm_2_list = []
-#         if i == province_path:
-#             for m in data['features']:
-#                 type_adm_1 = m['properties']['ENGTYPE_1']
-#                 if type_adm_1 not in type_adm_1_list:
-    #                 type_adm_1_list.append(type_adm_1)
-#         elif i == district_path:
-#             for m in data['features']:
-#                 type_adm_2 = m['properties']['ENGTYPE_2']
-#                 if type_adm_2 not in type_adm_2_list:
-    #                 type_adm_2_list.append(type_adm_2)  
---
+++ test.py
@@ -0,0 +1,4 @@
+import json
+with open('H:/WORK/Upwork/Project 7 - Python School Data Analysis/Countries Admin Levels/GADM-Countries/Extracted/Admin Data GeoJSON/United Kingdom/gadm36_GBR_3.geojson') as f:
+        data = json.load(f)
+
